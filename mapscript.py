6129038701eca814e2f6a3cf77d5a05d891b5fb4
Michael Weinberg <mwweinberg@users.noreply.github.com>
2018-07-25 21:21:28 -0400

    writes updated json to new file

--- mapscript.py
+++ mapscript.py
@@ -6,6 +6,8 @@ from pprint import pprint
 import pycountry
 import collections
 
+#TODO: automatically download the live oshwa list as the input file 
+
 #turns the csv into a list of lists [[x, y, z,], [a, b, c]]
 exampleFile = open('oshwalist.csv')
 exampleReader = csv.reader(exampleFile)
@@ -36,13 +38,11 @@ counter = collections.Counter(country_counter_list)
 print(counter)
 print(counter['BGR'])
 
-#TODO: replace the original holder data with the new holder data
-#probably something like country_data["features"][i]["properties"] =  holder
-#reversing how you pulled the info into the loop in the first place.
+# goes through each country and fills in the appropriate number of registrations
 for i in range(0, len(country_data["features"])):
     #outputs something like {'ISO_A3': 'ABW', 'ADMIN': 'Aruba'}
     holder = country_data["features"][i]["properties"]
-    print(holder)
+    #print(holder)
     #if the country code is in the counter list
     if holder['ISO_A3'] in counter:
         #make oshwa_count equal to the corresponding value
@@ -50,18 +50,11 @@ for i in range(0, len(country_data["features"])):
     else:
         #make oshwa_count equal to zero
         holder['oshwa_count'] = 0
-    #adds another pair along the lines of
-    #{'ISO_A3': 'ABW', 'oshwa_count': 3, 'ADMIN': 'Aruba'}
-    #holder['oshwa_count'] = 3
-    print(holder)
-'''
-#this is just to remember how this works when you are builidng the loop above
-holder = country_data["features"][0]["properties"]
-print(holder)
-holder['oshwa_count'] = 3
-print(holder)'''
+    #print(holder)
 
+    #now that the info is updated, replace the original with the new
+    country_data["features"][i]["properties"] =  holder
 
-
-
-#pprint(holder['ISO_A3'])
+#once all of the countries are done, write it to a new output file
+with open('output.json', 'w') as outfile:
+    json.dump(country_data, outfile)
